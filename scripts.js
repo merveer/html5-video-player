5148ec6e692fed18ddf88bdc4beb513054dea32a
Merve Erdol <merve-uygun@windowslive.com>
2017-10-23 17:20:05 +0300

    Fix lint errors

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-//get elements
+// get elements
 const player = document.querySelector('.player');
 const video = player.querySelector('.viewer');
 const progress = player.querySelector('.progress');
@@ -7,51 +7,51 @@ const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
 
-//build functions
-function togglePlay(){
-    const method = video.paused ? 'play' : 'pause';
-    video[method]();
+// build functions
+function togglePlay() {
+  const method = video.paused ? 'play' : 'pause';
+  video[method]();
 }
 
-function updateButton(){
-    const icon = this.paused ? '►' : '❚ ❚';
-    toggle.textContent = icon;
+function updateButton() {
+  const icon = this.paused ? '►' : '❚ ❚';
+  toggle.textContent = icon;
 }
 
-function skip(){ //how much to skip? go back to index.html button data-skip
-console.log(this.dataset.skip);
-video.currentTime += parseFloat(this.dataset.skip);
+function skip() { // how much to skip? go back to index.html button data-skip
+  console.log(this.dataset.skip);
+  video.currentTime += parseFloat(this.dataset.skip);
 }
 
-function handleRangeUpdate(){
-    video[this.name] = this.value;
-    console.log(this.value);
-    console.log(this.name);
+function handleRangeUpdate() {
+  video[this.name] = this.value;
+  console.log(this.value);
+  console.log(this.name);
 }
 
-function handleProgress(){
-const percent = (video.currentTime / video.duration) * 100;
-progressBar.style.flexBasis = `${percent}%`;
+function handleProgress() {
+  const percent = (video.currentTime / video.duration) * 100;
+  progressBar.style.flexBasis = `${percent}%`;
 }
 
-function scrub(e){
-    const scrubTime = (e.offsetX / progress.offsetWidth)*video.duration;
-    video.currentTime = scrubTime;
-    console.log(e);
+function scrub(e) {
+  const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+  video.currentTime = scrubTime;
+  console.log(e);
 }
 
-//hook up the event listeners
+// hook up the event listeners
 video.addEventListener('click', togglePlay);
-video.addEventListener('play',updateButton);
-video.addEventListener('pause',updateButton);
-video.addEventListener('timeupdate',handleProgress);
-toggle.addEventListener('click',togglePlay);
-skipButtons.forEach(button => button.addEventListener('click',skip));
-ranges.forEach(range => range.addEventListener('change',handleRangeUpdate));
-ranges.forEach(range => range.addEventListener('mousemove',handleRangeUpdate));
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+video.addEventListener('timeupdate', handleProgress);
+toggle.addEventListener('click', togglePlay);
+skipButtons.forEach(button => button.addEventListener('click', skip));
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
+ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
 
 let mousedown = false;
 progress.addEventListener('click', scrub);
-progress.addEventListener('mousemove', (e) => mousedown &&scrub(e));
+progress.addEventListener('mousemove', e => mousedown && scrub(e));
 progress.addEventListener('mousedown', () => mousedown = true);
 progress.addEventListener('mouseup', () => mousedown = false);
