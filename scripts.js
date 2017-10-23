9c06c9226587e6ccd3c18471becf9894035cc470
Merve Erdol <merve-uygun@windowslive.com>
2017-10-23 15:27:33 +0300

    Change play icon

--- scripts.js
+++ scripts.js
@@ -13,5 +13,13 @@ function togglePlay(){
     video[method]();
 }
 
+function updateButton(){
+    const icon = this.paused ? '►' : '❚ ❚';
+    toggle.textContent = icon;
+}
+
 //hook up the event listeners
-toggle.addEventListener('click', togglePlay);
+video.addEventListener('click', togglePlay);
+video.addEventListener('play',updateButton);
+video.addEventListener('pause',updateButton);
+toggle.addEventListener('click',togglePlay);
